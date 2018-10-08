de6d3dcd9cab13d146375941a746ce9dff63f8fd
Puumanamana <carisdak@hawaii.edu>
2018-10-07 22:51:29 -1000

    ...

--- evolution.py
+++ evolution.py
@@ -1,40 +1,89 @@
 #!/usr/bin/env python
 
-from model import Mapping
+from mapping import Mapping
 from time import time
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from multiprocessing.pool import Pool
+
 np.random.seed(1234)
-N_MARKER = 25
+N_MARKER = 28
 sequences = pd.read_csv('./sim1.csv',index_col=0)
 
 def timer(fun):
-    def fun_wrapper(x):
+    def fun_wrapper(*args,**kwargs):
         t0 = time()
-        fun(x)
+        fun(*args,**kwargs)
         print('{0} - Elapsed time: {1:.3f}'.format(fun.__name__,time()-t0))
     return fun_wrapper
+
+def evaluate(mapping):
+    return mapping.evaluate(sequences)
+
+def recombine(mappings):
+    '''
+    Mapping recombination between N individuals
+    Input: mapping object with contigency table
+    '''
+    reads = np.arange(0,N_MARKER)
+
+    new_clusters = {}
+    new_assignments = {}
+    n_clusters = 0
+
+    for read in reads:
+        c = 0 # iterator through "new" clusters
+        selected = False
+        while not selected and c<n_clusters:
+            reads_c = new_clusters[c]
+            # isTogether() returns the number of reads in reads_c that are in the same cluster as read
+            # Needs to check if reads_c is empty
+            res = np.array([mapping.isTogether(read,reads_c) * mapping.fitness
+                            for mapping in mappings])
+            chooseYes = res.sum()
+            chooseNo = np.sum([ mappings[i].fitness
+                                for i in np.where(res.astype(int)==0)[0] ])
+            weights = np.array([chooseNo,chooseYes])
+            selected = np.random.choice([False,True],p=weights/weights.sum())
+
+            if selected:
+                new_clusters[c].append(read)
+                new_assignments[read] = c
+            c += 1
+
+        if not selected:
+            new_clusters[n_clusters] = [read]
+            new_assignments[read] = n_clusters
+            n_clusters += 1
+
+    new_assignments = pd.Series(new_assignments,name='cluster')
+    new_assignments.index.name = 'marker'
+    return new_assignments
 
 class Evolution:
       
     def __init__(self,N):
         self.pop_size = N
         self.populations = {i: Mapping(N_MARKER,ID=i) for i in range(N)}
-        self.recombine_prob = .4
+        self.recombine_prob = .5
         self.mutation_rate = .1
         self.cores = 15
         self.fitnesses = []
 
     @timer
     def calc_fitnesses(self):
-        fitnesses = []
-        for p in self.populations.values():
-            p.evaluate(sequences)
-            fitnesses.append(p.fitness)
-        self.fitnesses.append(fitnesses)
-            
+        pool = Pool(self.cores)
+        scores_list = list(map(evaluate,
+                               self.populations.values()))
+        pool.close()
+        for i,scores in enumerate(scores_list):
+            self.populations[i].scores = scores
+            self.populations[i].fitness = scores.mean()
+
+        self.fitnesses.append([p.fitness for p in self.populations.values()])
+
     def select_next_gen(self):
         fitnesses = np.array([ p.fitness for p in self.populations.values() ])
         fitnesses_inv = np.max(fitnesses)-fitnesses+0.0001
@@ -68,59 +117,24 @@ class Evolution:
             if np.random.uniform() < self.mutation_rate:
                 player.mutate()
 
-    def recombine(self,mappings):
-        '''
-        Mapping recombination between N individuals
-        Input: mapping object with contigency table
-        '''
-        reads = np.arange(0,N_MARKER)
-
-        new_clusters = {0:[]}
-        new_assignments = {}
-        n_clusters = 1
-
-        for read in reads:
-            c = 0
-            choice = False
-            while ~choice and c<n_clusters:
-                reads_c = new_clusters[c]
-                # isTogether() returns the number of reads in reads_c that are in the same cluster as read
-                # Needs to check if reads_c is empty
-                res = np.array([mapping.isTogether(read,reads_c) * mapping.fitness
-                                for mapping in mappings])
-                chooseYes = res.sum()
-                chooseNo = np.sum([ mappings[i].fitness
-                                    for i in np.where(res.astype(int)==0)[0] ])
-                weights = np.array([chooseNo,chooseYes])
-                choice = np.random.choice([False,True],p=weights/weights.sum())
-                c += 1
-
-                if choice:
-                    new_clusters[c].append(read)
-                    new_assignments[read] = c
-
-        if ~choice:
-            n_clusters += 1
-            new_clusters[n_clusters] = [read]
-            new_assignments[c] = n_clusters
-        
-        child = Mapping(N_MARKER,assignments=new_assignments)
-
-        return child
 
     @timer
     def make_next_generation(self):
         parent_indices,removed = self.select_next_gen()
         parent_list = [(self.populations[i1],self.populations[i2])
                        for i1,i2 in parent_indices]
-
-        children = []
-        for parents in parent_list:
-            child = self.recombine(parents)
-            children.append(child)
+        t0 = time()
+        pool = Pool(self.cores)
+        assignments = pool.map(recombine,parent_list)
+        pool.close()
+        print('Recombination: {}'.format(time()-t0))
             
-        for nb,child in enumerate(children):
-            child.id = removed[nb]
+        for nb,ass in enumerate(assignments):
+            child = Mapping(N_MARKER,
+                            ID=removed[nb],
+                            assignments=ass,
+                            initialize=False)
+
             self.populations[removed[nb]] = child
 
     def cycle(self):
@@ -130,25 +144,17 @@ class Evolution:
         print(np.max(self.fitnesses[-1]))
 
     def cycles(self,n_gen,n_plots=6):
-        fig,ax = plt.subplots(1,n_plots)
         for n in range(n_gen):
             print('Generation {}/{}'.format(n,n_gen))
             self.cycle()
-
-            if (n+1)%(n_gen/n_plots) == 0:
-                i_plot = int((n+1)/(n_gen/n_plots))-1
-                self.demo(ax=ax[i_plot])
+            if (n+1) % 5 == 0:
+                print(self.best().hashtable)
         return self
 
-    def demo(self,ax=None):
-        fitnesses = sorted(self.populations.items(),
-                           key=lambda x: x[1].fitness,
-                           reverse=True)
-        best_player = fitnesses[0][1]
-
-        best_player.plot_shoot(target=self.target,ax=ax)
-
-
+    def best(self):
+       i_max = np.argmax(self.fitnesses[-1])
+       return self.populations[i_max]
+       
     def display_fitness(self):
         fig,ax = plt.subplots()
         for i,v in enumerate(ev.fitnesses):
@@ -162,7 +168,7 @@ class Evolution:
 if __name__ == '__main__':
 
     ev = Evolution(100)
-    ev.cycles(200)
+    ev.cycles(100)
 
     ev.display_fitness()
 
--- mapping.py
+++ mapping.py
@@ -1,34 +1,51 @@
 #!/usr/bin/env python
 import numpy as np
-from scipy.stats import nbinom
+from scipy.stats import nbinom,poisson
 import pandas as pd
 
 def convert_params(mu, sigma):
     """
     Convert mean/dispersion parameterization of a negative binomial to the ones scipy 
     """
-    mu = max(1,mu)
-    var = max(1,sigma**2)
-    
+    # mu = max(1,mu)
+    # var = max(1.1,sigma**2)
+    if mu > sigma:
+        print('Invalid parameters: mu > sigma.')
+        sigma = mu+1
+        
+    var = sigma**2
     p = (var - mu) / var
     r = mu**2 / (var-mu)
     
     return r, 1-p
 
+def logNB1m(sequences,threshold=1e-1):
+    def _logNB1m(indices):
+        x = [sequences[i] for i in indices]
+        means = np.mean(x,axis=0)
+        y = [poisson.pmf(x,np.mean(x))]
+        # y = nbinom.logpmf(*convert_params(mu,std),x)
+        y[y<threshold] = threshold
+        y[y>1-threshold] = 1-threshold
+        return np.sum((np.log(1-y))**2) # returns -SUM{log(1-P[x=1])}
+    return _logNB1m
+
 class Mapping:
 
-    def __init__(self,N,ID=None,assignments={}):
+    def __init__(self,N,ID=None,assignments={},initialize=True):
         self.id = ID
         self.Nseq = N
         self.assignments = assignments
-        self.scores = np.array([])
-        self.mutation_frequency = 0.1
+        self.mutation_frequency = 0.3
         self.hashtable = None
         self.contigency_table = None
-        self.initialize(N)
+        if initialize:
+            self.initialize(N)
+        self.n_cluster = len(self.assignments.unique())
+        self.setHashTable()
 
     def initialize(self,N):
-        self.n_cluster = np.random.randint(2,N/4)
+        self.n_cluster = np.random.randint(N/2,N)
         # Make sure every cluster has at least one element
         firsts = np.random.choice(self.Nseq,self.n_cluster,replace=False)
         others = np.setdiff1d(range(self.Nseq),firsts)
@@ -39,17 +56,12 @@ class Mapping:
         for cluster,first in enumerate(firsts):
             self.assignments[first] = cluster
         self.assignments.index.name = 'marker'
-        self.setHashTable()
 
     def setHashTable(self):
         self.hashtable = (self.assignments
                           .reset_index()
                           .groupby('cluster')['marker']
                           .apply(list))
-        clusterId_absent = np.setdiff1d(range(self.n_cluster),self.hashtable.index)
-        empty_df = pd.Series([[]]*len(clusterId_absent),
-                             index=clusterId_absent)
-        self.hashtable = pd.concat([self.hashtable,empty_df])
 
     def setContigencyTable(self):
         tmp = pd.concat([self.assignments]*len(self.assignments),
@@ -58,19 +70,8 @@ class Mapping:
         self.contigency_table = (tmp == tmp.T).astype(int)
         
     def evaluate(self,sequences):
-        self.scores = []
-        
-        for cluster,idx_seqC in self.hashtable.items():
-            seqC = sequences.iloc[idx_seqC].T
-            params = seqC.apply([np.mean,np.std]).T
-            
-            log_likelihood_C = np.mean([nbinom.logpmf(*convert_params(mean,std),seq[1])
-                                        for (mean,std,seq) in
-                                        zip(params['mean'],params['std'],seqC.iterrows())])
-            if np.isnan(log_likelihood_C):
-                log_likelihood_C = -1e5
-            self.scores = np.append(self.scores,log_likelihood_C)
-        self.fitness = np.median(self.scores)
+        self.scores = self.hashtable.apply(logNB1m)
+        return self.scores
 
     def mutate(self):
         '''
@@ -79,21 +80,18 @@ class Mapping:
         - Move it to a new cluster
         '''
         
-        inv_fitness = np.max(self.scores)-self.scores+0.001
-        inv_fitness = [val if len(self.hashtable[cluster]) > 1 else 0
-                       for (cluster,val) in enumerate(inv_fitness)]
-        switch_probs = inv_fitness/np.sum(inv_fitness)
-        switched = np.random.choice(self.n_cluster,
+        inv_fitness = self.scores.max() - self.scores
+        switch_probs = inv_fitness / inv_fitness.sum()
+        switched = np.random.choice(switch_probs.index,
                                     int(self.mutation_frequency*self.n_cluster),
-                                    p=switch_probs)
-        markers = []
-        for cluster in switched:
-            marker = np.random.choice(self.hashtable[cluster])
-            markers.append(marker)
+                                    p=switch_probs.values)
+        markers = [np.random.choice(self.hashtable[cluster])
+                   for cluster in switched]
+        for marker in markers:
             self.assignments[marker] = self.n_cluster
+        self.n_cluster += int(len(switched)>0)
                 
         self.setHashTable()
-
 
     def isTogether(self,read,reads_c):
         if self.contigency_table is None:
